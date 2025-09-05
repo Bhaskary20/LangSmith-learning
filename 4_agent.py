b9c71cc872f7a6fd0faceea0dfb67f558d85d0da
Bhaskar Jyoti Deka <124321017+Bhaskary20@users.noreply.github.com>
2025-09-05 23:37:12 +0530

    Update 4_agent.py

--- 4_agent.py
+++ 4_agent.py
@@ -23,17 +23,17 @@ def get_weather_data(city: str) -> str:
 
 llm = ChatOpenAI()
 
-# Step 2: Pull the ReAct prompt from LangChain Hub
-prompt = hub.pull("hwchase17/react")  # pulls the standard ReAct agent prompt
 
-# Step 3: Create the ReAct agent manually with the pulled prompt
+prompt = hub.pull("hwchase17/react")  
+
+
 agent = create_react_agent(
     llm=llm,
     tools=[search_tool, get_weather_data],
     prompt=prompt
 )
 
-# Step 4: Wrap it with AgentExecutor
+
 agent_executor = AgentExecutor(
     agent=agent,
     tools=[search_tool, get_weather_data],
@@ -41,11 +41,7 @@ agent_executor = AgentExecutor(
     max_iterations=5
 )
 
-# What is the release date of Dhadak 2?
-# What is the current temp of gurgaon
-# Identify the birthplace city of Kalpana Chawla (search) and give its current temperature.
 
-# Step 5: Invoke
 response = agent_executor.invoke({"input": "What is the current temp of gurgaon"})
 print(response)
 
